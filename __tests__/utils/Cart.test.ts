b03f113ee7c5c894831915c40476ada19639645e
dtun <dtun@me.com>
2024-04-15 08:13:00 -0700

    Update cart tests - mock async storage

--- __tests__/utils/Cart.test.ts
+++ __tests__/utils/Cart.test.ts
@@ -1,4 +1,12 @@
-import { getCartSum } from '@/utils/cart';
+import { getCartSum, loadCart, storeCart } from '@/utils/cart';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+jest.mock('@react-native-async-storage/async-storage', () => {
+  return {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+  };
+});
 
 describe('Cart', () => {
   test('getCartSum', () => {
@@ -8,5 +16,38 @@ describe('Cart', () => {
     ];
 
     expect(getCartSum(cart)).toBe(300);
+  });
+
+  test('returns the correct cart item', () => {
+    const cart = [
+      { id: 1, price: 100, name: 'item 1', quantity: 2 },
+      { id: 2, price: 200, name: 'item 2', quantity: 1 },
+    ];
+
+    storeCart(cart);
+
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith(
+      'cart',
+      JSON.stringify(cart)
+    );
+
+    loadCart();
+
+    expect(AsyncStorage.getItem).toHaveBeenCalledTimes(1);
+  });
+
+  test('loads the correct item', async () => {
+    const cart = [
+      { id: 1, price: 100, name: 'item 1', quantity: 2 },
+      { id: 2, price: 200, name: 'item 2', quantity: 1 },
+    ];
+
+    AsyncStorage.getItem = jest
+      .fn()
+      .mockResolvedValueOnce(JSON.stringify(cart));
+
+    const result = await loadCart();
+
+    expect(result).toMatchObject(cart);
   });
 });
